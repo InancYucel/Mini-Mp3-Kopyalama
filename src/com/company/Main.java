ea8c579f723d0acc81ab3445d188148be596927f
Inanc Yucel <inancyucel@gmail.com>
2021-12-10 14:24:46 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,57 @@
+package com.company;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Main {
+
+    private static ArrayList<Integer> icerik = new ArrayList<Integer>();
+
+    public static void dosyaOku(){
+
+        try {
+            FileInputStream in = new FileInputStream("Evgeny Grinko Valse.mp3");
+
+            int oku;
+
+            while ((oku = in.read()) != -1){
+
+                icerik.add(oku);
+
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void kopyala(String dosyaIsmi){
+
+        try {
+            FileOutputStream out = new FileOutputStream(dosyaIsmi);
+
+            for (int deger : icerik){
+
+                try {
+                    out.write(deger);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+         dosyaOku();
+         kopyala("Valse2.mp3");
+    }
+}
